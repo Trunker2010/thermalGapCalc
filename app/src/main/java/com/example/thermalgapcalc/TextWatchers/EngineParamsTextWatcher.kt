a124ac6a7cc563b65952d8e18a298b92571fc9f1
Trunker2010 <Trunker2010@users.noreply.github.com>
2021-06-09 18:37:52 +0300

    в текстовые поля настроек двигателя установлены значения по умолчанию.

--- app/src/main/java/com/example/thermalgapcalc/TextWatchers/EngineParamsTextWatcher.kt
+++ app/src/main/java/com/example/thermalgapcalc/TextWatchers/EngineParamsTextWatcher.kt
@@ -2,9 +2,11 @@ package com.example.thermalgapcalc.TextWatchers
 
 import android.text.Editable
 import android.text.TextWatcher
+import com.example.thermalgapcalc.DEFAULT_PARAM
 import com.example.thermalgapcalc.POINT_SIG
 
 class EngineParamsTextWatcher(val textChange: (CharSequence?) -> Unit) : TextWatcher {
+
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
     }
@@ -15,6 +17,8 @@ class EngineParamsTextWatcher(val textChange: (CharSequence?) -> Unit) : TextWat
     override fun afterTextChanged(s: Editable?) {
         if (s!!.isNotEmpty() && s.toString() != POINT_SIG) {
             textChange(s)
+        }else{
+            textChange(DEFAULT_PARAM)
         }
     }
 }
